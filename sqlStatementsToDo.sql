9887bd05b68fb6e45822bd31ca2a77b1e9e9eb8b
Vogt Janina <vogt.janina@gmx.de>
2023-06-23 10:27:17 +0200

    add more content to sql statements

--- sqlStatementsToDo.sql
+++ sqlStatementsToDo.sql
@@ -61,6 +61,11 @@ insert into myList values ( 500, '5', to_date('2023-11-11', 'yyyy-mm-dd'),'Liste
 insert into myList values ( 600, '4', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 6' );
 insert into myList values ( 700, '4', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 7' );
 insert into myList values ( 800, '4', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 8' );
+insert into myList values ( 900, '3', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 9' );
+insert into myList values ( 1000, '3', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 10' );
+insert into myList values ( 1100, '3', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 11' );
+insert into myList values ( 1200, '3', to_date('2023-04-05', 'yyyy-mm-dd'),'Liste 12' );
+
 
 
 insert into userRights values ( 1, 100, 1);
@@ -83,6 +88,13 @@ insert into listEntry (entryId, listId, responsible, creator, creationDate,endTi
 insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (3, 100, 1, 4, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Programmieren'); 
 insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (4, 100, 1, 4, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Abgabe'); 
 insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (5, 100, 1, 1, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Präsentation vorbereiten'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (6, 400, 1, 4, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Präsentation vorbereiten'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (7, 900, 4, 1, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Lernen'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (8, 200, 3, 1, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Prüfung scheiben'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (9, 1000, 3, 4, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Programmieren'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (10, 1100, 1, 3, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Abgabe'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (11, 1100, 3, 1, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Präsentation vorbereiten'); 
+insert into listEntry (entryId, listId, responsible, creator, creationDate,endTime,status, toDo) values (12, 200, 3, 4, to_date('2023-02-01', 'yyyy-mm-dd'), to_date('2023-08-01', 'yyyy-mm-dd'), 1, 'Präsentation vorbereiten'); 
 
 
 commit;
@@ -94,8 +106,15 @@ select u.*, l.*
 from myUser u, mylist l, userRights ur
 where u.myUserId = ur.myuser
 and ur.listid = l.listid;
-
 select * from userRights;
 
 select * from myUser; 
 select * from myList; 
+select * from ListEntry; 
+
+select * from ListEntry where listId = 400 and (responsible = 4 or creator = 4); 
+select * from ListEntry where listId = 200 and (responsible = 3 or creator = 3); 
+
+
+commit;
+
